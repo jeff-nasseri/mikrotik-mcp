2628deede088ed1fea7148c5aecd6144d03b65d8
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-07-31 18:03:27 +0200

    set key_filename to empty string

--- src/mcp_mikrotik/settings/configuration.py
+++ src/mcp_mikrotik/settings/configuration.py
@@ -1,12 +1,11 @@
 from dataclasses import dataclass
-from typing import Optional
 
 @dataclass
 class MikrotikConfig:
     host: str = "127.0.0.1"
     username: str = "admin"
     password: str = ""
-    key_filename: Optional[str] = None
+    key_filename: str = ""
     port: int = 22
 
 mikrotik_config = MikrotikConfig()
